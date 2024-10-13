d7502af2fe2b7bbbc27717ac843b5c3605b17de7
dkssu <dkssudrhd@naver.com>
2024-10-13 23:10:28 +0900

    feature : entityTest 수정

--- src/main/java/com/example/jenkins/repository/SchoolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jenkins.repository;
-
-import com.example.jenkins.entity.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School, Long> {
-    void createSchool(School school);
-}
--- src/main/java/com/example/jenkins/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jenkins.repository;
-
-import com.example.jenkins.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    void createStudent(Student student);
-}
